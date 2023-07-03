0352516fdb569876e07f0781ac1bd0f00c84bc23
Saksham Misra <122719607+vinfinity7@users.noreply.github.com>
2023-07-03 22:51:13 +0530

    redirect commit 2

--- public/styles.js
+++ public/styles.js
@@ -37,7 +37,7 @@ async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "https://spotify-saksham.netlify.app/dist/index.html");
+    params.append("redirect_uri", "https://benevolent-puffpuff-87e6bb.netlify.app/dist/index.html");
     params.append("scope", "user-read-private user-read-email user-read-playback-state user-top-read playlist-read-private user-read-recently-played");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -72,7 +72,7 @@ async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "https://spotify-saksham.netlify.app/dist/index.html");
+    params.append("redirect_uri", "https://benevolent-puffpuff-87e6bb.netlify.app/dist/index.html");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
