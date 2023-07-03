71792699af759b61fe4ffbb9b4179d24728beff8
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-07-03 22:24:25 +0530

    commit 1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ export default defineConfig({
     supported: {
       'top-level-await': true //browsers can handle top-level-await features
     },
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: '/dist/index.html', // Make sure to specify the correct entry module
+    },
+  },
 })
